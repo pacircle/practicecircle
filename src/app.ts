d5797b4fb0f903cc3df442caec36be6021f9ca0b
wcypersonal <417177466@qq.com>
2018-12-13 22:18:51 +0800

    调整页面tabBar

--- src/app.ts
+++ src/app.ts
@@ -13,20 +13,20 @@ export default class extends MyApp {
     logs.unshift(Date.now())
     wxp.setStorageSync('logs', logs)
 
-    // // 登录
-    // let {code} = await wxp.login()
-    // console.log('微信 code %o', code) // 发送 code 到后台换取 openId, sessionKey, unionId
+    // 登录
+    let {code} = await wxp.login()
+    console.log('微信 code %o', code) // 发送 code 到后台换取 openId, sessionKey, unionId
 
-    // // 获取用户信息
-    // let setting = await wxp.getSetting()
-    // if (setting.authSetting['scope.userInfo']) { // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //   // 可以将 getUserInfo 返回的对象发送给后台解码出 unionId
-    //   let res = await wxp.getUserInfo()
-    //   console.log('微信 userInfo %o', res.userInfo)
-    //   this.store.userInfo = res.userInfo  // 将用户信息存入 store 中
-    // } else {
-    //   console.log('没有授权过')
-    // }
+    // 获取用户信息
+    let setting = await wxp.getSetting()
+    if (setting.authSetting['scope.userInfo']) { // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+      // 可以将 getUserInfo 返回的对象发送给后台解码出 unionId
+      let res = await wxp.getUserInfo()
+      console.log('微信 userInfo %o', res.userInfo)
+      this.store.userInfo = res.userInfo  // 将用户信息存入 store 中
+    } else {
+      console.log('没有授权过')
+    }
   }
 }
 
