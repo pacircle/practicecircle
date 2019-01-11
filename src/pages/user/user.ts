882e20296edcd6748699b8a04c7b84648979cce4
wcypersonal <417177466@qq.com>
2019-01-11 13:32:42 +0800

    修改用户页面接口

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -29,15 +29,15 @@ export default class extends MyPage {
     console.log(this.store.openid)
     let store:any = this.store;
     await wx.request({
-      url:"http://result.eolinker.com/2iwkBiged241c5a42bdfb8b083224dbf190f8b770cac539?uri=/user/history",
+      url:"http://127.0.0.1:7979/user/read",
       data: {
         openid: store.openid
       },
       method:'POST',
       success: function(res){
         console.log("status:",res)
-        if (res.data.status === 200){
-          store.hisInfos = res.data.hisList
+        if (res.data.state === 200){
+          store.hisInfos = res.data.readList
         } else {
           wx.showToast({
             title: '获取阅读历史失败，请检查网络',
@@ -56,14 +56,14 @@ export default class extends MyPage {
     })
 
     await wx.request({
-      url:"http://result.eolinker.com/2iwkBiged241c5a42bdfb8b083224dbf190f8b770cac539?uri=/user/article",
+      url:"http://127.0.0.1:7979/user/article",
       data: {
         openid: store.openid
       },
       method:'POST',
       success: function(res){
         console.log("status:",res)
-        if (res.data.status === 200){
+        if (res.data.state === 200){
           store.myArticles = res.data.articleList
         } else {
           wx.showToast({
@@ -110,15 +110,15 @@ export default class extends MyPage {
     })
 
     await wx.request({
-      url:"http://result.eolinker.com/2iwkBiged241c5a42bdfb8b083224dbf190f8b770cac539?uri=/user/collect",
+      url:"http://127.0.0.1:7979/user/collect",
       data: {
         openid: store.openid
       },
       method:'POST',
       success: function(res){
         console.log("status:",res)
-        if (res.data.status === 200){
-          store.collectInfos = res.data.collectInfos
+        if (res.data.state === 200){
+          store.collectInfos = res.data.collectList
         } else {
           wx.showToast({
             title: '获取我的收藏失败，请检查网络',
