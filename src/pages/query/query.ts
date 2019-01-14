2f3e505f530e4b424fce8d3c836fcb39cce2817d
wcypersonal <417177466@qq.com>
2019-01-14 17:38:48 +0800

    添加搜索功能

--- src/pages/query/query.ts
+++ src/pages/query/query.ts
@@ -5,7 +5,10 @@ var WxSearch = require('../../base/wxSearch/wxSearch.js')
 @pagify()
 export default class extends MyPage {
   data = {
-    value: ''
+    value: '',
+    searchList: [],
+    focus: true,
+    inputValue: "",
   }
 
   async onLoad(options: any) {
@@ -34,16 +37,27 @@ export default class extends MyPage {
     var that = this;
     WxSearch.wxSearchAddHisKey(that);
     console.log(WxSearch)
+    that.setDataSmart({
+      focus: false
+    })
+    that.sendSearch(that.data.inputValue)
   }
 
   wxSearchInput(e:any) {
     var that = this
     WxSearch.wxSearchInput(e,that);
+    console.log(e)
+    that.setDataSmart({
+      inputValue: e.detail.value
+    })
   }
 
   wxSerchFocus(e:any) {
     console.log('wxSerchFocus')
     var that = this
+    that.setDataSmart({
+      focus: true
+    })
     WxSearch.wxSearchFocus(e,that);
   }
 
@@ -57,6 +71,11 @@ export default class extends MyPage {
     console.log('wxSearchKeyTap')
     var that = this
     WxSearch.wxSearchKeyTap(e,that);
+    console.log(e.currentTarget.dataset.key)
+    that.setDataSmart({
+      focus: false
+    })
+    that.sendSearch(e.currentTarget.dataset.key)
   }
   wxSearchDeleteKey(e:any) {
     console.log('wxSearchDeleteKey')
@@ -72,5 +91,66 @@ export default class extends MyPage {
     console.log('wxSearchTap')
     var that = this;
     WxSearch.wxSearchHiddenPancel(that);
+  }
+
+  async sendSearch(searchInfo:String){
+    let store:any = this.store
+    let that:any = this
+    wx.request({
+      url: "http://127.0.0.1:7979/article/search",
+      data: {
+        searchInfo:searchInfo,
+        openid: store.openid, 
+      },
+      method: 'POST',
+      success: function(res){
+        console.log(res)
+          if (res.data.state === 200){
+              console.log(res.data)
+              store.searchList = res.data.searchList
+              that.setSearchArticle(store.searchList)
+          } else {
+            wx.showToast({
+              title: '搜获信息为空',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+      },
+      fail: function(ress){
+
+      }
+    })
+  }
+
+  async setSearchArticle(articles: Array<JSON>){
+    await this.setDataSmart({
+      searchList: articles
+    }) 
+    if (articles.length == 0){
+      wx.showToast({
+        title: '没有搜索到相关文章，请尝试搜索其他关键字',
+        icon: 'none',
+        duration: 2000
+      })
+    }
+  }
+
+  clickDetail(e:any){
+    console.log(e.target.dataset.info)
+    // this.app.$url.repdetail.go({
+    //   infos: JSON.parse(e.target.dataset.infos)
+    // })
+    wx.navigateTo({
+      url: '../repdetail/repdetail?info=' + JSON.stringify(e.target.dataset.info),
+      success: function(res){
+      },
+      fail: function(res){
+        wx.showModal({
+          title: '显示详情',
+          content: '页面跳转失败，请刷新页面后重试'
+        })
+      }
+    })
   }
 }
